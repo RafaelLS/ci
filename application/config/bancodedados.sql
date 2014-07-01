d216f10193f994333e2f33ac557edcf5aa062162
Aluno <Aluno@10.1.21.31>
2014-07-01 10:09:36 -0300

    Projeto finalizado

--- application/config/bancodedados.sql
+++ application/config/bancodedados.sql
@@ -36,17 +36,20 @@ CREATE TABLE IF NOT EXISTS `news` (
   `autor` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `slug` (`slug`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf16 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf16 AUTO_INCREMENT=8 ;
 
 --
 -- Extraindo dados da tabela `news`
 --
 
 INSERT INTO `news` (`id`, `title`, `slug`, `text`, `autor`) VALUES
-(1, 'brasil x mexio', 'brasil-mexico', 'Vamo lá Brasil', 'rafael'),
-(2, 'filipe ladrão', 'filipe-ladrao', 'Sabe de nada inocente', 'filipe'),
-(3, 'aula CI', 'aula-ci', 'CI', 'rafael'),
-(4, 'vamo lá Brasil', 'vamo-la-brasil', 'Vai!!!!!!!', 'filipe');
+(1, 'Brasil x Mexico', 'brasil-mexico', 'Infelizmente empatamos, 0 x 0 mas um jogo equilibrado.', 'rafael'),
+(2, 'Filipe ladrão', 'filipe-ladrao', 'Sabe de nada inocente', 'filipe'),
+(3, 'Aula CI', 'aula-ci', 'CI', 'rafael'),
+(4, 'Vamo lá Brasil', 'vamo-la-brasil', 'Torcida brasileira mostra ânimo e otimismo em sua seleção!', 'filipe'),
+(5, 'Brasil x Colombia', 'brasil-x-colombia', 'Quem vai para a semi-final??', 'rafael'),
+(6, 'Brasil x Chile', 'brasil-x-chile', 'O Brasil passou sufoco mas com o esforço do goleiro ao defender os pênaltis conseguiu a vitória.', 'filipe'),
+(7, 'Aula CI', 'aula-ci', 'Continuação CodeIgniter....', 'tnx');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
@@ -55,5 +58,4 @@ INSERT INTO `news` (`id`, `title`, `slug`, `text`, `autor`) VALUES
 GRANT USAGE ON *.* TO 'ci'@'localhost' IDENTIFIED BY PASSWORD '*91DC9B36DC87190436224DA6B4279C3E4ECB397E';
 
 GRANT ALL PRIVILEGES ON `ci`.* TO 'ci'@'localhost';
-
 
